fc8403ea14e255b3493ff5b268d1b781e6bfe8ed
Rengumin <tranxuanphong26525@gmail.com>
2025-06-30 13:39:35 +0700

    feat: product-category

--- product-category/src/test/java/com/ecom/productcategory/ProductCategoryApplicationTests.java
+++ product-category/src/test/java/com/ecom/productcategory/ProductCategoryApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProductCategoryApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
