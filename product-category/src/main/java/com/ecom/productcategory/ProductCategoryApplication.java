bfc258f14879b1f344cbc6306d7a31588d021c65
Rengumin <tranxuanphong26525@gmail.com>
2025-06-30 13:39:35 +0700

    product-category

--- product-category/src/main/java/com/ecom/productcategory/ProductCategoryApplication.java
+++ product-category/src/main/java/com/ecom/productcategory/ProductCategoryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProductCategoryApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ProductCategoryApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCategoryApplication.class, args);
+	}
 
 }
