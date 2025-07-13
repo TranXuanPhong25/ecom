fd44f5aac6ffa80e59778be70490144b3488747f
Rengumin <tranxuanphong26525@gmail.com>
2025-07-13 09:43:48 +0700

    feat(eas): example gRpc connect

--- kong/plugins/eas-inbound-filter/go.mod
+++ kong/plugins/eas-inbound-filter/go.mod
@@ -2,9 +2,16 @@ module eas-inbound-filter
 
 go 1.23.10
 
-require github.com/Kong/go-pdk v0.11.0
+require (
+	github.com/Kong/go-pdk v0.11.0
+	google.golang.org/grpc v1.73.0
+)
 
 require (
 	github.com/ugorji/go/codec v1.2.12 // indirect
-	google.golang.org/protobuf v1.33.0 // indirect
+	golang.org/x/net v0.38.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250324211829-b45e905df463 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
 )
--- kong/plugins/eas-inbound-filter/main.go
+++ kong/plugins/eas-inbound-filter/main.go
@@ -6,22 +6,29 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
+	"bytes"
+	"context"
 	"github.com/Kong/go-pdk"
 	"github.com/Kong/go-pdk/server"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+	"time"
+
+	pb "eas-inbound-filter/proto"
 )
 
 func main() {
 	server.StartServer(New, Version, Priority)
 }
 
-var Version = "0.2"
+var Version = "0.1"
 var Priority = 1
 
 type Config struct {
-	Message string
+	Timeout         int    `json:"timeout"`
+	Message         string `json:"message"`
+	AuthServiceAddr string `json:"auth_service_addr"` // Địa chỉ gRPC service
 }
 
 func New() interface{} {
@@ -29,14 +36,64 @@ func New() interface{} {
 }
 
 func (conf Config) Access(kong *pdk.PDK) {
-	host, err := kong.Request.GetHeader("host")
-	if err != nil {
-		log.Printf("Error reading 'host' header: %s", err.Error())
+	var token string
+	authorizationHeader, getAuthorizationError := kong.Request.GetHeader("Authorization")
+	if getAuthorizationError == nil {
+		token = getTokenFromAuthorizationHeader(authorizationHeader)
+	}
+	cookieHeader, getCookieError := kong.Request.GetHeader("Cookie")
+	if getCookieError == nil {
+		token = getTokenFromCookieHeader(cookieHeader)
+	}
+	if token == "" {
+		kong.Response.Exit(401, []byte("Unauthorized access"), map[string][]string{
+			"Content-Type": {"application/json"},
+		})
+		return
 	}
 
-	message := conf.Message
-	if message == "" {
-		message = "hello"
+	//follow https://github.com/grpc/grpc-go/blob/master/examples/helloworld/greeter_client/main.go
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	conn, err := grpc.NewClient(conf.AuthServiceAddr, opts...)
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
 	}
-	kong.Response.SetHeader("x-hello-from-go", fmt.Sprintf("Go says %s to %s", message, host))
+	defer func(conn *grpc.ClientConn) {
+		err := conn.Close()
+		if err != nil {
+			log.Fatalf("fail to close connection: %v", err)
+		}
+	}(conn)
+
+	client := pb.NewJWTServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := client.ValidateToken(ctx, &pb.TokenRequest{Token: token})
+	if err != nil {
+		log.Fatalf("could not validate token: %v", err)
+	}
+	log.Printf("the token is: %v", r.GetValid())
+}
+
+func getTokenFromAuthorizationHeader(authorizationHeader string) string {
+	if len(authorizationHeader) > 7 && authorizationHeader[:7] == "Bearer " {
+		return authorizationHeader[7:]
+	}
+	return ""
+}
+func getTokenFromCookieHeader(cookieHeader string) string {
+	tokenField := []string{"token", "Token", "TOKEN"}
+	// Parse the cookie header to extract the token
+	cookieParts := bytes.Split([]byte(cookieHeader), []byte("; "))
+	for _, part := range cookieParts {
+		for _, field := range tokenField {
+			if bytes.HasPrefix(part, []byte(field+"=")) {
+				return string(part[len(field)+1:])
+			}
+		}
+	}
+	return ""
 }
