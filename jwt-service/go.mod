bc26850f69208bbebdc926d81ff5f296fc64f281
Rengumin <tranxuanphong26525@gmail.com>
2025-07-13 12:02:25 +0700

    feat(jwt-service): register JWTService gRPC server

--- jwt-service/go.mod
+++ jwt-service/go.mod
@@ -3,21 +3,13 @@ module github.com/TranXuanPhong25/ecom/jwt-service
 go 1.23.10
 
 require (
-	github.com/labstack/echo/v4 v4.13.4
 	google.golang.org/grpc v1.73.0
 	google.golang.org/protobuf v1.36.6
 )
 
 require (
-	github.com/labstack/gommon v0.4.2 // indirect
-	github.com/mattn/go-colorable v0.1.14 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/crypto v0.38.0 // indirect
 	golang.org/x/net v0.40.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
-	golang.org/x/time v0.11.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250324211829-b45e905df463 // indirect
 )
--- jwt-service/main.go
+++ jwt-service/main.go
@@ -1,14 +1,41 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/TranXuanPhong25/ecom/jwt-service/proto"
+	"google.golang.org/grpc"
+)
+
+type JWTService struct {
+	pb.UnimplementedJWTServiceServer
+}
+
+func (s *JWTService) ValidateToken(ctx context.Context, in *pb.TokenRequest) (*pb.ValidationResponse, error) {
+	log.Printf("Received token: %s", in.Token)
+	// Thêm logic xác thực token thực tế ở đây
+	return &pb.ValidationResponse{Valid: true}, nil
+}
+
+var (
+	RPC_PORT = ":8202" // gRPC server port
 )
 
 func main() {
-	e := echo.New()
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
+	lis, err := net.Listen("tcp", RPC_PORT)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
 
-	e.Logger.Fatal(e.Start(":8202"))
+	s := grpc.NewServer()
+	pb.RegisterJWTServiceServer(s, &JWTService{})
+
+	log.Printf("gRPC server listening on %v\n", RPC_PORT)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+	lis.Close()
+	s.Stop()
 }
