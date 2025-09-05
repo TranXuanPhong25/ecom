1704bb2402f151c0814561b5de34a95a39f6f733
Rengumin <tranxuanphong26525@gmail.com>
2025-09-05 17:25:57 +0700

    refactor(shops): return correct status code for entity notfound

--- shops/services/shops_service.go
+++ shops/services/shops_service.go
@@ -1,12 +1,15 @@
 package services
 
 import (
+	"errors"
+	"net/http"
+	"strings"
+
 	"github.com/TranXuanPhong25/ecom/shops/dtos"
 	"github.com/TranXuanPhong25/ecom/shops/models"
 	"github.com/TranXuanPhong25/ecom/shops/repositories"
 	"github.com/labstack/echo/v4"
-	"net/http"
-	"strings"
+	"gorm.io/gorm"
 )
 
 func CreateShop(request *models.CreateShopRequest) (models.Shop, echo.HTTPError) {
@@ -30,11 +33,17 @@ func CreateShop(request *models.CreateShopRequest) (models.Shop, echo.HTTPError)
 func GetShop(ownerId string) (*dtos.ShopDTO, *echo.HTTPError) {
 	shop := &models.Shop{}
 	tx := repositories.DB.First(shop, "owner_id = ?", ownerId)
+
 	if tx.Error != nil {
+		// Kiểm tra nếu là lỗi record not found
+		if errors.Is(tx.Error, gorm.ErrRecordNotFound) {
+			return nil, echo.NewHTTPError(http.StatusNotFound, "Shop not found")
+		}
+		// Các lỗi khác (database connection, SQL syntax, etc.)
 		return nil, echo.NewHTTPError(http.StatusInternalServerError, tx.Error.Error())
 	}
-	return toShopDTO(shop), nil
 
+	return toShopDTO(shop), nil
 }
 
 func toShopDTO(shop *models.Shop) *dtos.ShopDTO {
