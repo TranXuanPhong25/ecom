6f8ea846f5277ddf4e711c46e745fe1f80e3f159
Rengumin <tranxuanphong26525@gmail.com>
2025-10-01 18:50:16 +0700

    refactor: suppress compiler warning

--- products/src/main/java/com/ecom/products/configs/MetricsInterceptor.java
+++ products/src/main/java/com/ecom/products/configs/MetricsInterceptor.java
@@ -3,6 +3,9 @@ package com.ecom.products.configs;
 import com.ecom.products.monitors.CustomMetricsService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -16,8 +19,8 @@ public class MetricsInterceptor implements HandlerInterceptor {
     }
 
     @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
-                                Object handler, Exception ex) {
+    public void afterCompletion(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+                                @NonNull Object handler, @Nullable Exception ex) {
         metricsService.trackHttpRequest(
                 request.getMethod(),
                 request.getRequestURI(),
