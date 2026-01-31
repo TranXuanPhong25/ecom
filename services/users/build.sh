ab6eb351091f2208b6b8c50f73d9497b83ac5903
TranXuanPhong25 <tranxuanphong26525@gmail.com>
2026-01-31 20:40:50 +0700

    feat: go build scripts

---
+++ services/users/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Build and push Users service to Docker Hub
+SERVICE_NAME="users-svc"
+DOCKER_USERNAME="rengumin"
+VERSION="1.0"
+
+echo "Building $SERVICE_NAME..."
+
+# Build the Docker image
+docker build -t $DOCKER_USERNAME/$SERVICE_NAME:$VERSION .
+docker save $DOCKER_USERNAME/$SERVICE_NAME:$VERSION | sudo k3s ctr images import -
+
+# # Push to Docker Hub
+# echo "Pushing $SERVICE_NAME to Docker Hub..."
+# docker push $DOCKER_USERNAME/$SERVICE_NAME:$VERSION
+kubectl rollout restart deployment users-service -n services
+
+echo "$SERVICE_NAME built and pushed successfully!"
