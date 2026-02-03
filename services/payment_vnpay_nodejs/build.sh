ac630f53c6ffada4999417169bae6e90c389ed81
TranXuanPhong25 <tranxuanphong26525@gmail.com>
2026-02-03 11:44:10 +0700

    feat(payment): vnpaydemocode

---
+++ services/payment_vnpay_nodejs/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Build and push Order Placement service to Docker Hub
+SERVICE_NAME="vouchers-svc"
+DOCKER_USERNAME="rengumin"
+VERSION="1.0"
+
+echo "Building $SERVICE_NAME..."
+
+# Build the Docker image
+docker build -t $DOCKER_USERNAME/$SERVICE_NAME:$VERSION . --target=release
+docker save $DOCKER_USERNAME/$SERVICE_NAME:$VERSION | sudo k3s ctr images import -
+
+# # Push to Docker Hub
+# echo "Pushing $SERVICE_NAME to Docker Hub..."
+# docker push $DOCKER_USERNAME/$SERVICE_NAME:$VERSION
+kubectl rollout restart deployment vouchers-service -n services
+
+echo "$SERVICE_NAME built and pushed successfully!"
